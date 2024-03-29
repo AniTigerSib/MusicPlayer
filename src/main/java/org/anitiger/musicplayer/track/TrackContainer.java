106c6b38a1537f355ad62a272e31d397e42eecec
AniTigerSib <khudoleev04@mail.ru>
2024-03-29 16:17:52 +0500

    develop/Added Track container abstract class

---
+++ src/main/java/org/anitiger/musicplayer/track/TrackContainer.java
@@ -0,0 +1,76 @@
+package org.anitiger.musicplayer.track;
+
+import java.io.Externalizable;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+abstract class TrackContainer implements Externalizable {
+    protected ArrayList<Track> tracks;
+
+    public TrackContainer() {
+        tracks = new ArrayList<>();
+    }
+    public void addTrack(Track track) {
+        tracks.add(track);
+    }
+    public Track getTrackById(long id) {
+        for (Track track : tracks) {
+            if (track.getTrackId() == id) {
+                return track;
+            }
+        }
+        return null;
+    }
+    public Track getTrackByTitle(String title) {
+        for (Track track : tracks) {
+            if (track.getTrackTitle().equals(title)) {
+                return track;
+            }
+        }
+        return null;
+    }
+    public Track removeTrackById(long id) {
+        Track trackToRemove = getTrackById(id);
+        tracks.remove(trackToRemove);
+        return trackToRemove;
+    }
+    public Track removeTrackByTitle(String title) {
+        Track trackToRemove = getTrackByTitle(title);
+        tracks.remove(trackToRemove);
+        return trackToRemove;
+    }
+    public void deleteTrackById(long id) {
+        Track trackToDelete = removeTrackById(id);
+    }
+    public void deleteTrackByTitle(String title) {
+        Track trackToDelete = removeTrackByTitle(title);
+    }
+    public void clear() {
+        tracks.clear();
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws java.io.IOException {
+        out.writeInt(tracks.size());
+        for (Track track : tracks) {
+            track.writeExternal(out);
+        }
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws java.io.IOException {
+        int size = in.readInt();
+        for (int i = 0; i < size; i++) {
+            Track track;
+            try {
+                track = new Track();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            track.readExternal(in);
+            tracks.add(track);
+        }
+    }
+}
